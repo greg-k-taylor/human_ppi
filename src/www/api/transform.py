2f53a70b89273044da7fee51d104b3f29f7662be
Greg Taylor <greg@spritzr.com>
2017-10-05 15:57:35 -0700

    Updated transform.py to robustly handle incomplete data.

--- src/www/api/transform.py
+++ src/www/api/transform.py
@@ -5,36 +5,82 @@ import logging
 
 class ESResultTransformer(ESResultTransformer):
 
-    class PPIFields(Enum):
+    # *****************************************************************************
+    # Enumerated class for ConsensusPathDB PPI field names
+    # *****************************************************************************
+    class PpiFields(object):
         CONF = 'interaction_confidence'
-        PARTICIPANTS = 'interaction_participants'
+        PART = 'interaction_participants'
         PUBS = 'interaction_publications'
         DBS = 'source_databases'
 
-    # Add Human_PPI specific result transformations
-    def clean_query_GET_response(self, res):
-        logging.debug("GREG-BEFORE - {}".format(res))
-        for i, hit in enumerate(res['hits']['hits']):
+    # *****************************************************************************
+    # Safe parse for interaction_confidence
+    # *****************************************************************************
+    def parse_confidence(self, res, i, hit):
+        if self.PpiFields.CONF in hit['_source']:
+            val = hit['_source'][self.PpiFields.CONF]
+            val = None if (val == "NA" or val == '') else float(val)
+            res['hits']['hits'][i]['_source'][self.PpiFields.CONF] = val
+        else:
+            logging.warning("Field interaction_confidence not returned for this entry.".format(hit['_source']))
+            res['hits']['hits'][i]['_source'][self.PpiFields.CONF] = None
+        return res
 
-            # confidence
-            val = hit['_source']['interaction_confidence']
-            val = None if val == "NA" else float(val)
-            res['hits']['hits'][i]['_source']['interaction_confidence'] = val
+    # *****************************************************************************
+    # Safe parse for interaction_participants
+    # *****************************************************************************
+    def parse_participants(self, res, i, hit):
+        # participants - list of strings
+        if self.PpiFields.PART in hit['_source']:
+            val = hit['_source'][self.PpiFields.PART].replace("_HUMAN", "")
+            res['hits']['hits'][i]['_source'][self.PpiFields.PART] = [val] if ',' not in val else val.split(',')
+        else:
+            logging.warning("Field interaction_participants not returned for this entry - {}".format(hit['_source']))
+            res['hits']['hits'][i]['_source'][self.PpiFields.PART] = []
+        return res
 
-            # participants - list of strings
-            val = hit['_source']['interaction_participants'].replace("_HUMAN", "")
-            res['hits']['hits'][i]['_source']['interaction_participants'] = [val] if ',' not in val else val.split(',')
-
-            # publications - list of integers
-            val = hit['_source']['interaction_publications']
+    # *****************************************************************************
+    # Safe parse for interaction_publications
+    # *****************************************************************************
+    def parse_publications(self, res, i, hit):
+        # publications - list of integers
+        if self.PpiFields.PUBS in hit['_source']:
+            val = hit['_source'][self.PpiFields.PUBS]
             pubs_strs = [val] if ',' not in val else val.split(',')
-            res['hits']['hits'][i]['_source']['interaction_publications'] = list(map(int, pubs_strs))
+            try:
+                pubs_ints = list(map(int, pubs_strs))
+            except ValueError:
+                pubs_ints = []
+                logging.warning("Publication Entry for this query is not valid - {}".format(pubs_strs))
+            res['hits']['hits'][i]['_source'][self.PpiFields.PUBS] = pubs_ints
+        else:
+            logging.warning("Field interaction_publications not returned for this entry - {}".format(hit['_source']))
+            res['hits']['hits'][i]['_source'][self.PpiFields.PUBS] = []
+        return res
 
-            # databases - list of strings
-            val = hit['_source']['source_databases']
-            res['hits']['hits'][i]['_source']['source_databases'] = [val] if ',' not in val else val.split(',')
+    # *****************************************************************************
+    # Safe parse for source_databases
+    # *****************************************************************************
+    def parse_databases(self, res, i, hit):
+        # databases - list of strings
+        if self.PpiFields.DBS in hit['_source']:
+            val = hit['_source'][self.PpiFields.DBS]
+            res['hits']['hits'][i]['_source'][self.PpiFields.DBS] = [val] if ',' not in val else val.split(',')
+        else:
+            logging.warning("Field source_databases not returned for this entry - {}".format(hit['_source']))
+            res['hits']['hits'][i]['_source'][self.PpiFields.DBS] = []
+        return res
 
-            logging.debug("{} - {}".format(i, res['hits']['hits'][i]['_source']['interaction_confidence']))
-        logging.debug("GREG-AFTER - {}".format(res))
+    # #############################################################################
+    # Add Human_PPI specific result transformations for query results
+    # #############################################################################
+    def clean_query_GET_response(self, res):
+        logging.info("Query transformation call on {} hits".format(len(res['hits']['hits'])))
 
+        for i, hit in enumerate(res['hits']['hits']):
+            res = self.parse_confidence(res, i, hit)
+            res = self.parse_participants(res, i, hit)
+            res = self.parse_publications(res, i, hit)
+            res = self.parse_databases(res, i, hit)
         return res
